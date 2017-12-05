ea4d140b0f7c841923ef4b8512342f5587ce7a9f
morleegan <mpeter15@hawk.iit.edu>
2017-12-05 10:53:34 -0600

    removed old thalia

--- thalia/test/order_test.py
+++
@@ -1,6 +0,0 @@
-from thalia.order import Orders
-
-
-def test_create_order():
-    order = Orders()
-    assert isinstance(order, Orders)
--- thalia/thalia.py
+++
@@ -1,5 +0,0 @@
-"""Top file of thalia, will be run when using the app"""
-from delivery import app
-
-if __name__ == '__main__':
-    app.run(port=8080)
